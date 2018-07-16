d18d1015fa52b48d60d680471cc8cc3d9a78c017
annavor <voroshchuk96@gmail.com>
2018-07-16 19:14:21 +0300

    Create silly_blocks.rb

---
+++ 05_silly_blocks/silly_blocks.rb
@@ -0,0 +1,13 @@
+def reverser(&block) 
+  words = yield.split(" ")
+  transformed_words = words.inject(""){ |sum, word| sum += word.reverse + " "}
+  transformed_words.strip
+end
+
+def adder(num=1, &block)
+  yield + num
+end
+
+def repeater(num=1, &block)
+  num.times { yield }
+end
